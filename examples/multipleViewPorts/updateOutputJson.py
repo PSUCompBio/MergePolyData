65147c354979c3e898c04574020fdcf2b7fd2530
Anil Das P V <anil.das.pv+git@gmail.com>
2020-10-08 12:19:57 +0530

    Update output script processes element id and brain regions
    Added provision for fine/coarse mesh

--- examples/multipleViewPorts/updateOutputJson.py
+++ examples/multipleViewPorts/updateOutputJson.py
@@ -1,12 +1,28 @@
 import sys
 import json
 import linecache
+import numpy as np
 
-jsonOutputFile = sys.argv[1]
+jsonInputFile = sys.argv[1]
+inputJson = json.loads(open(jsonInputFile).read())
+uid = inputJson["uid"]
+jsonOutputFile = uid + '_output.json'
+linearAcc = inputJson['simulation']['linear-acceleration']
+angularAcc = inputJson['simulation']['angular-acceleration']
+
+if 'xt' in linearAcc:
+    maxG = np.max(np.sqrt(np.array(linearAcc['xv'])**2+np.array(linearAcc['yv'])**2+np.array(linearAcc['zv'])**2))/9.81
+    maxT = np.max(np.sqrt(np.array(angularAcc['xv'])**2+np.array(angularAcc['yv'])**2+np.array(angularAcc['zv'])**2))
+else:
+    maxG = np.sqrt(np.array(linearAcc[0])**2+np.array(linearAcc[1])**2+np.array(linearAcc[2])**2)
+    if isinstance(angularAcc, list):
+        maxT = np.sqrt(np.array(angularAcc[0])**2+np.array(angularAcc[1])**2+np.array(angularAcc[2])**2)
+    else:
+        maxT = np.fabs(angularAcc)
 
 outputJson = json.loads(open(jsonOutputFile).read())
 meshType = outputJson["output-file"].split('_')[0]
-cellDataFile = meshType'_cellcentres.txt'
+cellDataFile = meshType + '_cellcentres.txt'
 
 # Part list 
 partList = ["msc", "stem", "cerebellum", "frontal", "parietal", "occipital",
@@ -21,7 +37,7 @@ threshInjuryMetrics = ["CSDM-10", "CSDM-15", "CSDM-30", "CSDM-5",
 # Loop over metric with single element output
 for metric in maxInjuryMetrics:
     maxLocation = int(outputJson[metric]["global-element-id"])
-    maxLine = linecache.getline('cellcentres.txt', maxLocation).split()
+    maxLine = linecache.getline(cellDataFile, maxLocation).split()
     outputJson[metric]["brain-region"] = maxLine[4]
     outputJson[metric]["location-femtech-reference"] = \
             [float(maxLine[1]), float(maxLine[2]), float(maxLine[3])]
@@ -34,10 +50,14 @@ for metric in threshInjuryMetrics:
     for part in partList:
         outputJson[metric][part] = []
     for loc in maxLocation:
-        maxLine = linecache.getline('cellcentres.txt', int(loc)).split()
+        maxLine = linecache.getline(cellDataFile, int(loc)).split()
         outputJson[metric][maxLine[4]].append([float(maxLine[1]), \
                 float(maxLine[2]), float(maxLine[3])])
     outputJson[metric].pop("global-element-id", None)
+
+# Add max quantities 
+outputJson["max-linear-acc-g"] = maxG
+outputJson["max-angular-acc-rads2"] = maxT
 
 # jstr = json.dumps(outputJson, indent = 2, sort_keys=False)
 # print(jstr)
